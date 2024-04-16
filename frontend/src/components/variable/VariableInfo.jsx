0a45cfe74a716f5e68a1eeff5eab4bd61bf27db9
Yori Verbist <yori.verbist12@gmail.com>
2024-04-16 16:30:48 +0200

    Fixed variable table

--- frontend/src/components/variable/VariableInfo.jsx
+++ frontend/src/components/variable/VariableInfo.jsx
@@ -11,6 +11,10 @@ export default function VariableInfo({
     (key) => !excludeKeys.includes(key),
   );
 
+  const uniqueValues = [
+    ...new Set(patients.map((patient) => patient[selectedValue])),
+  ];
+
   return (
     <>
       <div className="w-80 border-2 m-auto">
@@ -25,7 +29,22 @@ export default function VariableInfo({
         </Select>
       </div>
       <div className="border-2 w-80 m-auto rounded">
-        <p>{selectedValue}</p>
+        <table className="w-full">
+          <thead>
+            <tr className="flex w-full">
+              <th className="w-1/2 p-2">{selectedValue}</th>
+              <th className="w-1/2 p-2">Feature Importance</th>
+            </tr>
+          </thead>
+          <tbody className="flex flex-col items-center justify-between overflow-y-scroll w-full max-h-48">
+            {uniqueValues.map((value, index) => (
+              <tr key={index} className="flex w-full mb-2">
+                <td className="w-1/2">{value}</td>
+                <td className="w-1/2">0.5</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
