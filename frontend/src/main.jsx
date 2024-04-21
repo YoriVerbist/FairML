41679aa663e1924197a560d60dd516111ca214e4
Yori Verbist <yori.verbist12@gmail.com>
2024-04-21 19:18:16 +0200

    Added routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
 import { ThemeProvider } from "@material-tailwind/react";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>,
 );
