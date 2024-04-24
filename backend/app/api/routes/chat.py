182ce6f98ddc996974414baa7015719fa35c220d
Yori Verbist <yori.verbist12@gmail.com>
2024-04-24 17:27:27 +0200

    added try except again

--- backend/app/api/routes/chat.py
+++ backend/app/api/routes/chat.py
@@ -1,12 +1,11 @@
 from typing import Any
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 
 from app.models import OutputwithPayloadDataModel
 from app.api.routes.utils import (
     answer_question,
 )
-from app.core.db import fetch_user_details, update_user_details
 
 router = APIRouter()
 
@@ -18,19 +17,17 @@ def get_user_input(input: dict) -> Any:
     """
     Retrieve users.
     """
-    print(input)
     user = input["messages"][1]["user"]
     input["messages"][0]["text"] = input["messages"][0]["text"] + "in the dataset"
+
     if user not in CHAT_HISTORY:
         CHAT_HISTORY[user] = [input["messages"][0]["text"]]
     else:
         CHAT_HISTORY[user].append(input["messages"][0]["text"])
 
-    # try:
-    #     response = answer_question(input, chat_history=CHAT_HISTORY[user])
-    # except:
-    #     response = {"output": "Something went wrong. Please try again."}
-    print(CHAT_HISTORY[user])
-    response = answer_question(input, chat_history=CHAT_HISTORY[user])
-    print(response)
+    try:
+        response = answer_question(input, chat_history=CHAT_HISTORY[user])
+    except:
+        response = {"output": "Something went wrong. Please try again."}
+
     return {"text": response["output"]}
