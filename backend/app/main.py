a87617f5eadf239f87624d7783e31626f363a57e
Yori Verbist <yori.verbist12@gmail.com>
2024-05-10 22:08:18 +0200

    Fixed backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.main import api_router
 from app.core.config import settings
@@ -10,15 +10,11 @@ app = FastAPI(
     title=settings["PROJECT_NAME"],
 )
 
-origins = [
-    "http://localhost:5173",  # Local development
-    "https://yoriverbist.github.io/FairML/",  # Your deployed React app
-]
 
 # Set all CORS enabled origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
